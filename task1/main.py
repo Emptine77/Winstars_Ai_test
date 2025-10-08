9dbc13bd574fde497fe494450a0d93f2caf415b6
Olexii Hlushko <empt1ne77@gmail.com>
2025-10-08 16:26:11 +0300

    change test_size to 20%

--- task1/main.py
+++ task1/main.py
@@ -67,7 +67,7 @@ if __name__ == "__main__":
     X = mnist.data
     y = mnist.target    
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.8, random_state=42
+        X, y, test_size=0.2, random_state=42
     )
     mnist_classifier = MnistClassifier(classifier='rf')
     mnist_classifier.train(X_train, y_train)
